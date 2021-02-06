f7edd93d83d1efe2f9966d879f81c2c85accfb79
Brandon Navarro <55892338+kalani-nav@users.noreply.github.com>
2021-02-05 20:14:00 -0700

    Add files via upload

---
+++ falling ball/script.js
@@ -0,0 +1,121 @@
+var character = document.getElementById("character");
+var game = document.getElementById("game");
+var interval;
+var both = 0;
+var counter = 0;
+var level = 0.5;
+var currentBlocks = [];
+
+function moveLeft(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left>0){
+        character.style.left = left - 2 + "px";
+    }
+}
+
+function moveRight(){
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    if(left<380){
+        character.style.left = left + 2 + "px";
+    }
+}
+
+document.addEventListener("keydown", event => {
+    if(both==0){
+        both++;
+        if(event.key==="ArrowLeft"){
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.key==="ArrowRight"){
+            interval = setInterval(moveRight, 1);      
+        }
+    }
+});
+
+document.addEventListener("keyup", event => {
+    clearInterval(interval); 
+    both=0;
+});
+
+var blocks = setInterval(function(){
+    var blockLast = document.getElementById("block"+(counter-1));
+    var holeLast = document.getElementById("hole"+(counter-1));
+    if(counter>0){
+        var blockLastTop = parseInt(window.getComputedStyle(blockLast).getPropertyValue("top"));
+        var holeLastTop = parseInt(window.getComputedStyle(holeLast).getPropertyValue("top"));
+    }
+    if(blockLastTop<400||counter==0){
+        var block = document.createElement("div");
+        var hole = document.createElement("div");
+        block.setAttribute("class", "block");
+        hole.setAttribute("class", "hole");
+        block.setAttribute("id", "block"+counter);
+        hole.setAttribute("id", "hole"+counter);
+        block.style.top = blockLastTop + 100 + "px";
+        hole.style.top = holeLastTop + 100 + "px";
+        var random = Math.floor(Math.random() * 360);
+        hole.style.left = random + "px";
+        game.appendChild(block);
+        game.appendChild(hole);
+        currentBlocks.push(counter);
+        counter++;
+
+        // Beat the level
+        if(counter-9==25){
+            alert("Level Completed. Blocks cleared: 25");
+            level = 0.75;
+        }
+        if(counter-9==50){
+            alert("Level Completed. Blocks cleared: 50");
+            level = 1;
+        }
+        if(counter-9==75){
+            alert("Level Completed. Blocks cleared: 75");
+            level = 1.25;
+        }
+        if(counter-9==100){
+            alert("Level Completed. Blocks cleared: 100. Get ready for final boss level...");
+            level = 1.75;
+        }
+        if(counter-9==125){
+            alert("You won! Total boxes cleared: 125");
+            clearInterval(blocks);
+            location.reload();
+        }
+    }
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    var drop = 0;
+    if(characterTop <= 0){
+        alert("Game over. Score: " + (counter-9));
+        clearInterval(blocks);
+        location.reload();
+    }
+    for(var i = 0; i < currentBlocks.length; i++){
+        let current = currentBlocks[i];
+        let iblock = document.getElementById("block"+current);
+        let ihole = document.getElementById("hole"+current);
+        let iblockTop = parseFloat(window.getComputedStyle(iblock).getPropertyValue("top"));
+        let iholeLeft = parseFloat(window.getComputedStyle(ihole).getPropertyValue("left"));
+        iblock.style.top = iblockTop - level + "px";
+        ihole.style.top = iblockTop - level + "px";
+        if(iblock < -20){
+            currentBlocks.shift();
+            iblock.remove();
+            ihole.remove();
+        }
+        if(iblockTop-20<characterTop && iblockTop>characterTop) {
+            drop++;
+            if(iholeLeft<=characterLeft && iholeLeft+20>=characterLeft){
+                drop = 0;
+            }
+        }
+        if(drop==0){
+            if(characterTop < 480){
+                character.style.top = characterTop + 2 + "px";
+            }
+        }else{
+            character.style.top = characterTop - 0.5 + "px";
+        }
+    }
+}, 1)
